98320f06e4de0bd68e0fb137ed318bea24d4b9df
Paul Grant Truesdell, II <paul@pgt.sh>
2025-01-23 17:54:47 -0500

    Fix deprecation notices in PHP 8.4
    Signed-off-by: Paul Grant Truesdell, II <paul@pgt.sh>

--- src/helpers.php
+++ src/helpers.php
@@ -1,14 +1,14 @@
 <?php
 
 if (! function_exists('logsnag')) {
-    function logsnag(string $channel, string $event, string $description = null, string $icon = null, bool $notify = false): void
+    function logsnag(string $channel, string $event, ?string $description = null, ?string $icon = null, ?bool $notify = false): void
     {
         app(\PGT\Logsnag\Logsnag::class)->log($channel, $event, $description, $icon, $notify);
     }
 }
 
 if (! function_exists('insight')) {
-    function insight(string $title, string $value, string $icon = null): void
+    function insight(string $title, string $value, ?string $icon = null): void
     {
         app(\PGT\Logsnag\Logsnag::class)->insight($title, $value, $icon);
     }
